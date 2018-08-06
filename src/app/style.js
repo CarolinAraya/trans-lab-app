8f2c38aa096397468874cf95b8885610a3dec141
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-08-06 10:51:54 -0400

    integración sidebar

---
+++ src/app/style.js
@@ -0,0 +1,18 @@
+//funcionalidad del side Menú
+function toggleMenu() { // añadir función onclick="toggleMenu()" al botón del nav bar y al botón cerrar.
+  if (sideMenu.className.indexOf("menu_closed") >= 0) { // primero revisamos si la clase d-none esta
+    openMenu();  // si esta la clase quiere decir que el menú esta cerrado, asi que llamamos la funcion para abrirlo
+  } else {
+    closeMenu(); //si no esta la clase, le indicamos que cierre el menu
+  }
+}
+
+function openMenu() {
+  sideMenu.classList.remove('menu_closed'); // quitando clase display-none
+  sideMenu.classList.add('menu_open');
+}
+
+function closeMenu() {
+  sideMenu.classList.add('menu_closed'); // añadimos la clase display-none
+  sideMenu.classList.remove('menu_open');
+}
