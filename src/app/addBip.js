fd1e7af8655f7d0082cf4bbfffcf2315d1af52cd
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-08-06 10:20:12 -0400

    imprimiendo desde firebase

--- src/app/addBip.js
+++ src/app/addBip.js
@@ -1,10 +1,11 @@
 const codeBipInput = document.getElementById("add-bip");
 const addBipButton = document.getElementById("button-add-bip");
+const containerBip = document.getElementById("containerBips");
 
 let codeBip;
 
 const myBip = {
-    codeBip : null,
+    codeBip: null,
     key: null
 }
 
@@ -12,6 +13,11 @@ const addBip = () => {
     codeBip = codeBipInput.value;
     myBip.codeBip = codeBip;
     saveBipFirebase(myBip);
+    const bipContainer = document.createElement('div');
+    containerBips.appendChild(bipContainer)
+    bipContainer.innerHTML += `<div class="bip-code">
+                                <p>${myBip.codeBip}</p>    
+                               </div>`
 }
 
 const saveBipFirebase = () => {
@@ -19,9 +25,18 @@ const saveBipFirebase = () => {
         .ref().child('idBips')
         .push().key;//crea uno vacío para obtener la llave
 
-        myBip.key = newBipKey;
+    myBip.key = newBipKey;
 
     return firebase.database()
         .ref('idBips/' + newBipKey)
         .set(myBip); //set actualiza el valor en esta dirección
 }
+
+firebase.database().ref('idBips')
+    .on('child_added', (newBip) => {
+        const bipContainer = document.createElement('div');
+        containerBips.appendChild(bipContainer)
+        bipContainer.innerHTML += `<div class="bip-code">
+                                <p>${newBip.val().codeBip}</p>    
+                               </div>`
+    });
