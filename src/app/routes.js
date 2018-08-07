e9c6c4886acdb5e98ef7cf73c1c015278200cf0a
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-08-07 13:04:21 -0400

    avance en ruteo

---
+++ src/app/routes.js
@@ -0,0 +1,39 @@
+
+function showPerfilPage() {
+    $('#getResidueApp').hide();
+    $('#perfilApp').show();
+    $('#logged').show();
+
+  }
+  
+  function showGetResiduePage() {
+    $('#perfilApp').hide();
+    $('#getResidueApp').show();
+  }
+
+/*   function showRegisterPage() {
+    //$('#login').hide();
+    $('#passwordRegister').show();
+    register();
+  } */
+
+  function showRegisterPage() {
+    //$('#loginRegister').hide(); 
+   // $('#logged').hide(); 
+    $('#registerPage').show();
+}
+
+function showPerfilPage() {
+    $('#loginRegister').hide(); 
+    $('#logged').show(); 
+    $('#registerPage').hide();
+}
+
+function showLoginPage() {
+    $('#loginRegister').show(); 
+    $('#logged').hide(); 
+    $('#registerPage').hide();
+}
+
+ 
+ 
