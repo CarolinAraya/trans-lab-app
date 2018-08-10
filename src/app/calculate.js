001638c21330e9656dd8bb5f90794185e21329d5
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-08-10 10:43:00 -0400

    calculando tarifa terminado

--- src/app/calculate.js
+++ src/app/calculate.js
@@ -1,4 +1,5 @@
 passageContainer = document.getElementById("passage-container");
+finalValueContainer = document.getElementById("final-value-container");
 
 let tariffBip = "";
 let codeBipSelect = "";
@@ -6,8 +7,8 @@ let codeBipSelect = "";
 function getTariff(event) {
 
     tariffBip = event.target.value;
-    
-    passageContainer.innerHTML = `<div class= "residue">${ "$" + tariffBip}</div>`;
+
+    passageContainer.innerHTML = `<div class= "residue">${"$" + tariffBip}</div>`;
 
     return tariffBip;
 }
@@ -21,30 +22,31 @@ function getBip(event) {
 
 const calculateTariff = () => {
 
-     coincidedBip = null;
+    coincidedBip = null;
 
-   firebase.database().ref('userBips')
-                       .orderByChild("bip/id").equalTo(codeBipSelect)
-                       .limitToLast(1)
-                       .once("child_added", (snapshot) => {
-                        
-                        console.log(snapshot.val());
-                        coincidedBip = snapshot.val();
-            
-                        bipValue = coincidedBip.bip.saldoTarjeta;
-                        patron = "$";
-                        nuevovalor = "";
-                        balance = bipValue.replace(patron, nuevovalor);
-                        console.log(balance);
-                        patron = ".";
-                        nuevovalor = "";
-                        balanceFinal  = balance.replace(patron, nuevovalor); 
+    firebase.database().ref('userBips')
+        .orderByChild("bip/id").equalTo(codeBipSelect)
+        .limitToLast(1)
+        .once("child_added", (snapshot) => {
 
-                        totalBalance = balanceFinal - tariffBip; 
+            console.log(snapshot.val());
+            coincidedBip = snapshot.val();
 
-                        return totalBalance;
+            bipValue = coincidedBip.bip.saldoTarjeta;
+            patron = "$";
+            nuevovalor = "";
+            balance = bipValue.replace(patron, nuevovalor);
+            console.log(balance);
+            patron = ".";
+            nuevovalor = "";
+            balanceFinal = balance.replace(patron, nuevovalor);
 
-                    });
+            totalBalance = balanceFinal - tariffBip;
 
-                           
+            finalValueContainer.innerHTML = `<div class= "residue">${"$" + totalBalance}</div>`
+            return totalBalance;
+
+        });
+
+
 }
