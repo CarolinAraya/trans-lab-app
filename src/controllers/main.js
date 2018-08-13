f333da14594e47395bc06bf78629f74ccff7c1db
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-08-13 12:06:11 -0300

    estableciendo funciones para imprimir datos

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -2,25 +2,40 @@
 const codeBipInput = document.getElementById("add-bip");
 const containerBip = document.getElementById("containerBips");
 const selectBipContainer = document.getElementById("userBipsSelect");
-const getResidueSelect = document.getElementById("getResidueBipSelect");
 
 //calculate tariff
-
 passageContainer = document.getElementById("passage-container");
 finalValueContainer = document.getElementById("final-value-container");
-
 //get residue
-
 const form = document.getElementById("get-residue-bip");
 const bipKeyInput = document.getElementById("key-bip");
 const responseContainer = document.getElementById("response-container");
 
 
-var residueBip = "";
+let residue = "";
+let residueFromData = "";
 
 const addBip = () => {
-    
+
     codeBip = codeBipInput.value;
 
     getResidueFromApi(codeBip);// save future in local storage
+
+
+}
+
+const cleanResidueFromData = () => {
+
+    patron = "$";
+    newValue = "";
+    
+    residue = residueFromData.replace(patron, newValue);
+
+    domFunction(residue);
+}
+
+const domFunction = () => {
+
+    responseContainer.innerHTML = `<h1 class= "residue">${data.saldoTarjeta}</h1>`
+
 }
--- src/models/app.js
+++ src/models/app.js
@@ -4,9 +4,8 @@ getResidueFromApi = (codeBip) => {
     $.ajax({
         url: `http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=${codeBip}`
     }).done(data => {
-        residueBip = data.saldoTarjeta;
-       // responseContainer.innerHTML = `<h1 class= "residue">${data.saldoTarjeta}</h1>`
+        residueFromData = data.saldoTarjeta;
        
-        return residueBip;
+       cleanResidueFromData(residueFromData);
     });
 }
