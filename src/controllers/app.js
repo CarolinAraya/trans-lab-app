e6912e60c091a42a56db38f62fb06db8bdc6d483
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-08-13 09:57:12 -0300

    MVC

---
+++ src/controllers/app.js
@@ -0,0 +1,16 @@
+//add bip
+const codeBipInput = document.getElementById("add-bip");
+const containerBip = document.getElementById("containerBips");
+const selectBipContainer = document.getElementById("userBipsSelect");
+const getResidueSelect = document.getElementById("getResidueBipSelect");
+
+//calculate
+
+passageContainer = document.getElementById("passage-container");
+finalValueContainer = document.getElementById("final-value-container");
+
+//get residue
+
+const form = document.getElementById("get-residue-bip");
+const bipKeyInput = document.getElementById("key-bip");
+const responseContainer = document.getElementById("response-container");
