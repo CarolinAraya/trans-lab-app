9f672d6716b388ecd9af19ed523e5a53a93688d8
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-08-15 16:05:08 -0300

    primera estructura de validación

--- src/controllers/tariff-bip.js
+++ src/controllers/tariff-bip.js
@@ -1,3 +1,4 @@
+tariffBip = "";
 
 const tariffInput = () => {
 
@@ -18,5 +19,28 @@ function printTariff(event) {
 
     passageContainer.innerHTML = `<div class= "residue">${"$" + tariffBip}</div>`;
 
-    return tariffBip;
+}
+
+const userBipsSelectionChanged = (event) => {
+
+    let bipCard = event.target.value;
+    let tariff = selectTariff.value;
+
+    if (bipCard > 0 && tariff > 0) {
+
+        passageContainer.innerHTML = `<div class= "residue">${"$" + tariff}</div>`;
+
+        getBipAmountFromApi(bipCard, (amount) => {
+
+
+            finalValueContainer.innerHTML = `<p class= "residue">${"$" + (amount - (tariff))}</p>`
+
+        });
+    }
+    else {
+
+        passageContainer.innerHTML = "";
+        finalValueContainer.innerHTML = "";
+    }
+
 }
