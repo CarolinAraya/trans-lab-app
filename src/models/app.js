a946eaf94245f7951d611530778da2196b9a3e0b
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-10-14 16:58:50 -0300

    cambio https a api

--- src/models/app.js
+++ src/models/app.js
@@ -3,7 +3,7 @@
 getBipAmountFromApi = (codeBip, callback) => { 
    
     $.ajax({
-        url: `http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=${codeBip}`
+        url: `https://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=${codeBip}`
     }).done(data => {
         callback(data.saldoTarjeta.replace("$", "").replace(".", ""));
     });
