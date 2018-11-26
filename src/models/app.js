6997914a6b50bede882b7f435f5e02d32bfdd0c6
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-11-26 18:48:48 -0300

    errores ingresados

--- src/models/app.js
+++ src/models/app.js
@@ -1,12 +1,19 @@
 
 
-getBipAmountFromApi = (codeBip, callback) => { 
-   
+getBipAmountFromApi = (codeBip, callback) => {
     $.ajax({
-        url: `https://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=${codeBip}`
-    }).done(data => {
-        callback(data.saldoTarjeta.replace("$", "").replace(".", ""));
-    });
+        url: `https://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=${codeBip}`,
+        success: data => {
+            callback(data.saldoTarjeta.replace("$", "").replace(".", ""))
+
+        },
+        statusCode: {
+            400 : () => {
+                alert("Verifica que el código bip sea correcto")
+            },
+            500: () => {
+                alert("Ups! Tenemos problemas con el servidos. Disculpe! Intente más tarde.")
+            }
+        }
+    })
 }
-
-
